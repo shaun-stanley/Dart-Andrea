b63233beb465ef88839bc227738ff74c87676f21
Shaun Stanley <shaun.stanley_ug23@ashoka.edu.in>
2021-07-01 19:13:07 +0530

    initial commit

---
+++ ex_1.dart
@@ -0,0 +1,11 @@
+void main() {
+  String firstName = "Shaun";
+  String lastName = "Stanley";
+  int age = 19;
+  double height = 5.10;
+
+  print(firstName);
+  print(lastName);
+  print(age);
+  print(height);
+}
