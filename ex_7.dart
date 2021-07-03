0c6d3a40d37e6337f90f4fd2719889e350a9129f
Shaun Stanley <shaun.stanley_ug23@ashoka.edu.in>
2021-07-03 11:31:51 +0530

    initial commit

---
+++ ex_7.dart
@@ -0,0 +1,17 @@
+void main() {
+  const pos = 4;
+
+  // Write a program using 'if' and 'else' statements
+  // to display which medal the person recieves based on the
+  // value of the variable 'pos'
+
+  if (pos == 1) {
+    print('Gold🥇');
+  } else if (pos == 2) {
+    print('Silver🥈');
+  } else if (pos == 3) {
+    print('Bronze🥉');
+  } else {
+    print('Better luck next time! 😃');
+  }
+}
