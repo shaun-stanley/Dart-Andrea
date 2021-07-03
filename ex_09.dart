16123dccd7abb0197d3446f91635b69292ff1e53
Shaun Stanley <shaun.stanley_ug23@ashoka.edu.in>
2021-07-03 12:06:08 +0530

    initial commit

--- ex_09.dart
+++ ex_09.dart
@@ -1,1 +1,28 @@
+enum Medal {
+  gold,
+  silver,
+  bronze,
+  noMedal,
+}
+void main() {
+  const Medal medal = Medal.silver;
 
+  // Write a program using enumeration
+  // to display which medal the person recieves based on the
+  // value of the variable 'medal'
+
+  switch (medal) {
+    case Medal.gold:
+      print('Gold🥇');
+      break;
+    case Medal.silver:
+      print('Silver🥈');
+      break;
+    case Medal.bronze:
+      print('Bronze🥉');
+      break;
+    case Medal.noMedal:
+      print('Better luck next time! 😃');
+      break;
+  }
+}
