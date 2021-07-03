1cd37ed9e510bd2727254b74bb3bd58783ac1920
Shaun Stanley <shaun.stanley_ug23@ashoka.edu.in>
2021-07-03 11:35:45 +0530

    initial commit

---
+++ ex_8.dart
@@ -0,0 +1,21 @@
+void main() {
+  const pos = 2;
+
+  // Write a program using switch cases
+  // to display which medal the person recieves based on the
+  // value of the variable 'pos'
+
+  switch (pos) {
+    case 1:
+      print('Gold🥇');
+      break;
+    case 2:
+      print('Silver🥈');
+      break;
+    case 3:
+      print('Bronze🥉');
+      break;
+    default:
+      print('Better luck next time! 😃');
+  }
+}
